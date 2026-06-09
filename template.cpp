e69ffa7e7de5aa20c82dd7fe2e794597f8f452e2
Harsha30ywffb7 <hvgulla@phizzle.com>
2026-06-09 16:14:29 +0530

    cpp template added

---
+++ template.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// Typedefs
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+
+// Constants
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+// Macros
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+
+// Modular exponentiation
+ll binpow(ll a, ll b, ll mod = MOD) {
+    ll res = 1;
+    a %= mod;
+    while (b) {
+        if (b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+
+// GCD / LCM
+ll gcdll(ll a, ll b) {
+    return b ? gcdll(b, a % b) : a;
+}
+
+ll lcmll(ll a, ll b) {
+    return a / gcdll(a, b) * b;
+}
+
+void println(vec<int> a){
+    for(int x: a) cout<<x<" ";
+}
+
+
+void solve() {
+    // Write solution here
+
+}
+
+int main() {
+    fastio();
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
